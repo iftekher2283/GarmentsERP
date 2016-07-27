a5a88d0d06071e27ce85991a33be7f7d4cfa8dad
Git User <volume420@gmail.com>
2016-07-27 16:49:50 +0600

    IE Design

---
+++ src/IEPanelUIController.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+
+/**
+ * FXML Controller class
+ *
+ * @author student
+ */
+public class IEPanelUIController implements Initializable {
+
+    @FXML
+    private Text ieIdBulletinText;
+    @FXML
+    private Text ieIdCapacityText;
+    @FXML
+    private Text ieIdProductionText;
+    @FXML
+    private Text ieIdManpowerText;
+    @FXML
+    private Text ieIdMonitorText;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleBulletinSignOutAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleCapacitySignOutAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleProductionSignOutAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleManpowerSignOutAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleMonitorSignOutAction(ActionEvent event) {
+    }
+    
+}
